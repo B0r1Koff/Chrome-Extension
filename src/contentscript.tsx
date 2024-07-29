7efc9d37ce97eb1f771771eb988afe8d01007da9
B0r1Koff <vborikov2607@gmail.com>
2024-07-29 18:11:33 +0300

    final version

--- src/contentscript.tsx
+++ src/contentscript.tsx
@@ -5,17 +5,20 @@ import { hydrateRoot } from "react-dom/client";
 
     const createButton = (openModal: () => void) => {
         const button = document.createElement('button');
-        button.innerText = 'Click me';
+        button.innerText = 'Q';
         button.style.marginLeft = '10px';
-        button.style.backgroundColor = 'blue';
+        button.style.backgroundColor = 'red';
         button.style.color = 'white';
         button.style.border = 'none';
         button.style.padding = '5px 10px';
         button.style.cursor = 'pointer';
+        button.style.fontSize = 'xx-large'
+        button.style.height = '50px'
+        button.style.width = '150px'
+        button.style.fontWeight = '700'
       
-        // Добавляем обработчик событий для кнопки
         button.addEventListener('click', (event) => {
-          event.preventDefault(); // Предотвращаем стандартное поведение ссылки
+          event.preventDefault(); 
           openModal()
         });
       
@@ -29,10 +32,6 @@ const Modal = () => {
     setIsModalOpen(prevState => !prevState);
   };
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
   useEffect(() => {
     for (let i = 0; i < items.length; i++) {
       const element = items[i] as HTMLElement;
@@ -44,34 +43,45 @@ const Modal = () => {
 
   return (
     <div>
-      <ModalWindow isOpen={isModalOpen} onClose={closeModal}/>
+      <ModalWindow isOpen={isModalOpen}/>
     </div>
   );
 };
 
 interface ModalProps {
   isOpen: boolean;
-  onClose: () => void  
 }
 
-const ModalWindow = ({isOpen, onClose} : ModalProps) => {
-  let [selectedCity, setSelectedCity] = useState("Минск")
- 
+const ModalWindow = ({isOpen} : ModalProps) => {
+  let [selectedCity, setSelectedCity] = useState<string>("Минск")
+  let [temperature, setTemperature] = useState(0)
+
   useEffect(() => {
-    chrome.runtime.onMessage.addListener((msg) => {
-        setSelectedCity(msg)
+    chrome.runtime.onMessage.addListener((message) => {
+        setSelectedCity(message)
     })
+    fetch(`https://www.meteosource.com/api/v1/free/point?place_id=${getCity(selectedCity)}&sections=all&timezone=auto&language=en&units=metric&key=247mt6lo25aypjjn0m2qld5mv6e2hre0rnux057h`)
+    .then(response => response.json())
+    .then(data => {
+        setTemperature(data.current.temperature)
+    })
+    .catch(error => alert('Ошибка при получении данных о погоде:' + error));
   }, [selectedCity])
 
   return (
-    <div style={{background: 'red', display: `${isOpen ? 'block' : 'none'}`, width: '300px', height: '100px', position: 'fixed', left: '50%', top: '50%', zIndex: '9999'}} className="modal">
-      <div className="modal-content" style={{width: '100%', height: '100%'}}>
-        <button onClick={() => onClose()}>close</button>
-        <p>{selectedCity}</p>
+    <div style={{background: 'rgb(255, 220, 220)', display: `${isOpen ? 'block' : 'none'}`, width: '300px', height: '120px', position: 'fixed', left: '50%', top: '50%', zIndex: '9999'}} className="modal">
+      <div id="weather" className="modal-content" style={{width: '100%', height: '100%'}}>
+        <p style={{fontSize: 'x-large', fontWeight: '700', color: 'black', padding: '5px 0 0 10px'}}>Погода в {selectedCity}е на сегодня <br/>{temperature > 0 && "+"}{Math.round(temperature)}</p>
       </div>
     </div>
   );
 };
+
+function getCity(city: string){
+  if(city === "Минск") return "Minsk"
+  if(city === "Брест") return "Brest"
+  else return "Vitebsk"
+}
 
 const container = document.createElement('div');
 document.body.appendChild(container);
--- src/popupPage/popup.tsx
+++ src/popupPage/popup.tsx
@@ -9,9 +9,7 @@ export const Popup: FC<Props> = () => {
     let [city, setCity] = useState<string>("Минск")
 
     const sendRequest = (value: string) => {
-        chrome.storage.sync.set({ city: value }).then(() => {
-            alert(`Установлено значение: ${value}`)
-        });
+        chrome.storage.sync.set({ city: value })
         chrome.tabs.query({currentWindow: true, active: true}, tabs => {
             const currentTabId = tabs.length === 0 ? 0 : tabs[0].id!
             chrome.tabs.sendMessage(currentTabId, value, response => {
@@ -25,21 +23,12 @@ export const Popup: FC<Props> = () => {
         sendRequest(value)
     } 
 
-    // useEffect(() => {
-    //     chrome.tabs.query({currentWindow: true, active: true}, tabs => {
-    //         const currentTabId = tabs.length === 0 ? 0 : tabs[0].id!
-    //         chrome.tabs.sendMessage(currentTabId, city, response => {
-    //             console.log("Response from content: ", response);
-    //         })
-    //     })
-    // }, [])
-
     return(
         <div>
             <select name="" id="" value={city} onChange={(e) => {handleCityChange(e.target.value)}}>
                 <option value="Минск">Минск</option>
                 <option value="Брест">Брест</option>
-                <option value="Гомель">Гомель</option>
+                <option value="Витебск">Витебск</option>
             </select>
         </div>
     )
